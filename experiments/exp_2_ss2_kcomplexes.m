798920c085486497f204aa4fb01eb0db8c304aa4
Slasnista <stan.chambon@gmail.com>
2018-07-19 18:32:24 -0700

    update experiments section

---
+++ experiments/exp_2_ss2_kcomplexes.m
@@ -0,0 +1,69 @@
+% run spinky on spindle detection
+
+%% Example
+% Set parametres 
+clear 
+clc
+
+%matlab -nodesktop -nosplash
+
+addpath('functions');
+
+% loop over record
+data_path = '/home/infres/schambon/Papers/mcsleep/data/final_SS2/mat_files/';
+fil=fullfile(data_path,'*.mat')
+d=dir(fil)
+
+epoch_length=30; 
+fs=256;
+thresholds = linspace(0, 250, 11);
+
+
+p = parpool('local', 20); 
+
+for k=1:numel(d)  
+  file_name=fullfile(data_path,d(k).name)
+
+  f = load(file_name)
+
+  tr_data=data_epoching(f.chan,fs*epoch_length);
+
+  sizes = size(f.chan);
+
+  metrics = cell(11, 1);
+
+  for i_th=1:numel(thresholds)  
+    th = thresholds(i_th)
+
+    % run parallel prediction
+    prediction = cell(length(tr_data),1);
+    parfor i=1:length(tr_data)
+      [a, b] = sp_detection(tr_data{i}, th, fs);
+
+      if a == 0
+        b = zeros(1, fs * epoch_length);
+      end
+      prediction{i} = b;
+    end
+
+    % create binary vector
+    y_pred = zeros(1, sizes(1, 2));
+    for i=1:length(tr_data)
+      y_pred((i - 1) * fs * epoch_length + 1:i * fs * epoch_length) = prediction{i};
+    end
+
+    % compute metrics over differend gold standard
+    metrics{i_th} = compute_f1(f.kcomplex, y_pred, fs);
+    metrics{i_th}.threshold = th;
+
+  end
+
+  a = strsplit(file_name, '/');
+  b = a{1, end};
+  c = strsplit(b, '.');
+
+  save(['scores/SS2/expe_2_ss2_kcomplexes_' c{1} '.mat'], 'metrics')
+
+end
+
+delete(gcp('nocreate'))
